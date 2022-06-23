8ce62d48f7c78b2a6fb2f3ddffdb34714bd7cd99
Lapis <45414333+Lapis256@users.noreply.github.com>
2022-06-24 02:47:12 +0900

    remove parseManifest

--- src/types/manifest.ts
+++
@@ -1,7 +0,0 @@
-export interface Module {
-    type: "data" | "script" | "resource";
-}
-
-export interface Manifest {
-    modules: Module[];
-}
--- src/utils.ts
+++ src/utils.ts
@@ -1,16 +1,7 @@
 import { workspace } from "vscode";
 
-import { readFile } from "fs/promises";
-import { parse } from "jsonc-parser/lib/esm/main";
-
 import { EXTENSION_NAME } from "./constants";
-import { Manifest } from "./types/manifest";
 
 export function getConfiguration<T>(section: string): T | undefined {
     return workspace.getConfiguration(EXTENSION_NAME).get<T>(section);
-}
-
-export async function parseManifest(path: string): Promise<Manifest> {
-    const text = await readFile(path, "utf-8");
-    return parse(text);
 }
